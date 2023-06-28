8e5f7f84cb935b8f99dee12acd87b7f649732a01
Ntsako Baloyi <ST10127198@rcconnect.edu.za>
2023-06-28 19:32:32 +0200

    [api] setting up routes

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,15 @@
-const express = require('express');
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const express = require("express");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const userRouter = require("./routes/user");
+const tasksRouter = require("./routes/tasks");
+
 const app = express();
 
 //Adding morgan
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
 //JSON format by default
 app.use(express.json());
@@ -13,8 +17,6 @@ app.use(express.json());
 //Adjusting the CORS
 app.use(cors());
 
-app.use('/', (req, res, next)=>{
-  console.log(req.body);
-});
+//Routes
 
 app.listen(3000);
