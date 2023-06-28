118b5424c186b0c17a29f9c1968bbdd386067a02
Ntsako Baloyi <ST10127198@rcconnect.edu.za>
2023-06-28 19:23:26 +0200

    [api] adjusting CORS

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
 
 //Adding morgan
@@ -8,6 +9,9 @@ app.use(morgan('dev'));
 
 //JSON format by default
 app.use(express.json());
+
+//Adjusting the CORS
+app.use(cors());
 
 app.use('/', (req, res, next)=>{
   console.log(req.body);
