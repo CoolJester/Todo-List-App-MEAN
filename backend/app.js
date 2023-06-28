1406c41ec54d60e1da24a2e5d27d3eb459db27f3
Ntsako Baloyi <ST10127198@rcconnect.edu.za>
2023-06-28 19:19:03 +0200

    [api] parse requests to JSON

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 const app = express();
 
 //Adding morgan
 app.use(morgan('dev'));
+
+//JSON format by default
+app.use(express.json());
+
+app.use('/', (req, res, next)=>{
+  console.log(req.body);
+});
 
 app.listen(3000);
