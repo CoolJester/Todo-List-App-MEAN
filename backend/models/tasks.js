933cea8d0e8ebe25fbdec4defbbcd60571e204f3
Ntsako Baloyi <ST10127198@rcconnect.edu.za>
2023-06-30 10:30:55 +0200

    [api] tasks model

--- backend/models/tasks.js
+++ backend/models/tasks.js
@@ -1,1 +1,27 @@
-const 
+const mongoose = require("mongoose");
+
+const tasksSchema = mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "Please set a default name for the task"],
+      trim: true,
+    },
+    date: {
+      type: Date,
+      default: Date.now(),
+    },
+    notes: [],
+    status: {
+      type: String,
+      default: "busy",
+    },
+    userId: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
