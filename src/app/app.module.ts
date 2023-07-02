63d6140936e5f1bb7af306a89547fba59831e44a
Ntsako Baloyi <ST10127198@rcconnect.edu.za>
2023-07-02 06:53:37 +0200

    [frontend] authService creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -18,7 +19,7 @@ import { AppRouterModule } from './app.router.module';
     RegisterComponent,
     MainComponent,
   ],
-  imports: [BrowserModule, AppRouterModule],
+  imports: [BrowserModule, AppRouterModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
