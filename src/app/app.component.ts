932f1237b9c2e9249cb3e1084ebae56f36d7d26d
Ntsako Baloyi <ST10127198@rcconnect.edu.za>
2023-07-05 16:52:40 +0200

    [frontend] auto login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Todo-List';
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    //Auto login
+    if (localStorage.getItem('token')) {
+      console.log('Not Logged in');
+      if (localStorage.getItem('token') !== 'null') {
+        this.router.navigate['/main'];
+      }
+    }
+  }
 }
