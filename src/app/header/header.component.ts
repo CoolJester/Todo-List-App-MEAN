2ce05558c877443cb1209f9259239424227866ce
Ntsako Baloyi <ST10127198@rcconnect.edu.za>
2023-07-07 17:26:04 +0200

    [frontend] logout functionality

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HeaderService } from '../shared/services/header.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -15,7 +16,7 @@ export class HeaderComponent implements OnInit {
   //helper variables
   profileMenu = false;
 
-  constructor(public headerService: HeaderService) {}
+  constructor(public headerService: HeaderService, private router: Router) {}
 
   ngOnInit(): void {
     // Changing the header content dynamically
@@ -39,5 +40,13 @@ export class HeaderComponent implements OnInit {
   toggleProfileMenu() {
     console.log('Works');
     this.profileMenu = !this.profileMenu;
+  }
+
+  logout() {
+    //remove the token
+    localStorage.removeItem('token');
+    alert('Logged out');
+    //redirect to home
+    this.router.navigate(['/']);
   }
 }
